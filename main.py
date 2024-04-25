36fea0a45657f64ca150f2665f5d6ceae660e57f
David Eidelman <eidelman.david@gmail.com>
2024-04-25 15:24:54 -0400

    confirmed that postgres docker works

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 """
 main.py
 """
+from sqlalchemy import create_engine
 
+db_name= 'ragsc'
+db_user = 'postgres'
+db_pass = 'postgres'
+db_host = 'localhost'
+db_port = '5432'
+
+db_string = f'postgresql+psycopg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
+db = create_engine(db_string)
+
+
+def test_postgres():
+    print("testing postgres")
+    print("test completed")
 
 def main():
     print("hello from ragsc")
+    test_postgres()
 
 
 if __name__ == "__main__":
