af280c339300d00badc3a0d672bfe94aeffb29d5
David Eidelman <eidelman.david@gmail.com>
2024-04-28 18:07:28 -0400

    markdown loading and chunking

--- src/ragsc/__main__.py
+++ src/ragsc/__main__.py
@@ -1,4 +1,43 @@
+import argparse
+import os
+import sys
 from loguru import logger
+from ragsc import embed
+from pathlib import Path
+from dotenv import load_dotenv
 
-if __name__ == '__main__':
+load_dotenv()
+
+DATAPATH_ENV = "RAGSC_DATAPATH"
+
+if DATAPATH_ENV in os.environ:
+    data_path = os.environ[DATAPATH_ENV]
+else:
+    data_path = "data"
+
+
+def process_data(data_path_str: str) -> None:
+    data_path = Path(data_path_str)
+    if data_path.exists() and data_path.is_dir():
+        embed.process_data(data_path)
+    else:
+        logger.error("{} is not a valid path to a directory".format(data_path.absolute().as_posix()))
+        sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="ragsc",
+        description="Retrieval augmented generation proof of concept",
+        epilog="Copyright 2024, David Eidelman.  MIT License.",
+    )
+    parser.add_argument("-d", "--data", metavar="DATAPATH", default=data_path)
+    args = parser.parse_args()
+    logger.info(f"The datafile provided = {args.data}")
+
+    process_data(args.data)
+
+
+if __name__ == "__main__":
     logger.info("Executing the ragsc package")
+    main()
---
+++ src/ragsc/embed.py
@@ -0,0 +1,35 @@
+from os import PathLike
+from pathlib import Path
+from langchain_core.documents import Document
+from loguru import logger
+from langchain_community.document_loaders.markdown import UnstructuredMarkdownLoader
+from tqdm import tqdm
+
+
+def load_markdown_documents(path: Path) -> list[Document]:
+    docs = []
+    files = list(path.glob("*.md"))
+    for item in tqdm(files):
+        loader = UnstructuredMarkdownLoader(
+            item.as_posix(), mode="elements", strategy="fast"
+        )
+        docs.append(loader.load())
+        # docs.append(loader.load_and_split(RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)))
+    logger.info("found %d markdown docs" % len(docs))
+    logger.debug(len(docs[0]))
+    return docs
+
+
+def sum_documents(docs: list[Document]) -> int:
+    sum = 0
+    for doc in tqdm(docs):
+        sum += len(doc)  # type: ignore
+    return sum
+
+
+def process_data(path: PathLike):
+    logger.info("processing markdown documents at %s" % str(path))
+    path = Path(path)
+    docs = load_markdown_documents(path)
+    logger.info(f"total chunks: {sum_documents(docs)}")
+    return docs
