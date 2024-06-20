1b0b7f3f706e61acaf30a87db4a8aa9ade2db4a0
deidelma <eidelman.david@gmail.com>
2024-06-20 15:03:22 +0000

    Corrected batch bug.

--- embedit.py
+++ embedit.py
@@ -22,6 +22,7 @@ import json
 import concurrent.futures
 import threading
 import time
+import shutil
 
 
 dotenv.load_dotenv(".env")
@@ -75,12 +76,18 @@ def get_embedding(cell_no, gene_signature) -> tuple[int, str]:
 
 def process_concurrently(df: pd.DataFrame, start: int = 0, num_rows: int = 5):
     futures = {}
-    for row in range(start, num_rows):
+    for row in range(start, start+num_rows):
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
             futures[row] = executor.submit(get_embedding, row, df.signature[row])
 
     for future in futures:
-        df.embeddings[future] = json.dumps(futures[future].result()[1])
+        # if future % 5 == 0:
+            # print(futures[future].result()[0])
+        row_no, embedding = futures[future].result()
+        embedding = json.dumps(embedding)
+        print(row_no, embedding[0:10])
+        # df.embeddings[row_no] = embedding
+        # df.embeddings[futures[future].result()] = json.dumps(futures[future].result()[1])
 
 
 def process_embeddings(df: pd.DataFrame):
@@ -90,26 +97,49 @@ def process_embeddings(df: pd.DataFrame):
 
 
 def batch_process(df: pd.DataFrame, batch_size: int):
+    """Process the rows of the provided dataframe as batches.
+
+    Args:
+        df (pd.DataFrame): The dataframe to process.
+        batch_size (int): The batch size.
+    """
     n_rows = df.shape[0]
-    batch_size = 100
+    batch_size = 10
     cycles = n_rows // batch_size
     extra = n_rows - cycles * batch_size
     logger.info(
-        "Processing {} rows, divided into {} batches of size {}, followed by remaining {} rows",
-        n_rows,
-        cycles,
-        batch_size,
-        extra,
+        "Processing {} rows, divided into {} batches of size {}, followed by remaining {} rows", n_rows, cycles, batch_size, extra,
     )
     # process the batches up to the remainder
-    for start_index in range(0, (cycles + 1) * batch_size, batch_size):
-        process_concurrently(df, start_index, batch_size)
-        if start_index % batch_size == 0:
-            print(f"{start_index}")
-    print(f"adding {extra} from {cycles * batch_size} to {cycles * batch_size + extra}")
+    if cycles > 0:
+        print(f"running from 0 to {cycles*batch_size} by {batch_size}")
+        for start_index in range(0, cycles * batch_size, batch_size):
+            if start_index >= n_rows:
+                break
+            process_concurrently(df, start_index, batch_size)
+            if start_index % batch_size == 0:
+                logger.info(f"Processed {start_index} rows")
     # process the remainder
-    process_concurrently(df, cycles * batch_size, extra)
+    if extra > 0:
+        print(f"extr cycles:{cycles} batch size {batch_size} extra {extra} cycles*batch_size {cycles*batch_size}")
+        process_concurrently(df, cycles * batch_size, extra)
+    logger.info("added {extra} rows from {cycles * batch_size} to {cycles * batch_size + extra}")
 
+def save_dataset(df: pd.DataFrame):
+    """Save dataset to a csv, making a backup of the previous data file.
+
+    Args:
+        df (pd.DataFrame): The dataframe to save to disk.
+    """
+    data_file = Path(INPUT_FILE)
+    bu_file = Path(INPUT_FILE + '.bak')
+    logger.info("saving data file: {} backing up to: {}", data_file, bu_file)
+    if data_file.exists():
+        if bu_file.exists():
+            bu_file.unlink()
+        shutil.copy(data_file, bu_file)
+    df.to_csv(data_file)
+    logger.info("dataset saved to {}", data_file)
 
 def main():
     global api_key
@@ -119,9 +149,12 @@ def main():
     api_key = os.getenv("OPENAI_API_KEY")
     logger.debug("API_KEY:{}", api_key)
     df = load_dataset()
-    # process_concurrently(df)
-    # batch_process(df, 100)
-    # # print(df.head())
+    df = df[df.index < 37]
+    # process_concurrently(df, 0,10)
+    batch_process(df, 100)
+    # print(df.head(20))
+    # save_dataset(df)
+    
     logger.info("shutting down")
 
 
