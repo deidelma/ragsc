bb07fad0d721d5e5ecb79a4c491f40250d48d5b2
deidelma <eidelman.david@gmail.com>
2024-06-20 18:36:39 +0000

    prepped to go

--- embedit.py
+++ embedit.py
@@ -7,15 +7,10 @@ likely to be folded intoto ragsc.embed once completed
 """
 
 import os
-import gzip
-
-# from typing import Tuple
-# import openai
 import dotenv
 import pandas as pd
 from pathlib import Path
 
-# from openai import OpenAI
 from loguru import logger
 import requests
 import json
@@ -58,7 +53,16 @@ def download_site(url):
 empty_json = json.dumps([])
 
 
-def get_embedding(cell_no, gene_signature) -> tuple[int, str]:
+def get_embedding(cell_no: int, gene_signature: str) -> tuple[int, str]:
+    """Get the embedding for the gene_signature.
+
+    Args:
+        cell_no (int): The row or cell number to be associated with this embedding.
+        gene_signature (str): The gene signature for this cell.
+
+    Returns:
+        tuple[int, str]: The cell (row) number and the embedding as a tuple.
+    """
     if not isinstance(gene_signature, str):
         logger.error("attempting to embed non-string data: {}", gene_signature)
         return cell_no, empty_json
@@ -74,20 +78,26 @@ def get_embedding(cell_no, gene_signature) -> tuple[int, str]:
         return cell_no, empty_json
 
 
-def process_concurrently(df: pd.DataFrame, start: int = 0, num_rows: int = 5):
+def process_concurrently(df: pd.DataFrame, start: int = 0, num_rows: int = 5) -> None:
+    """Get the embeddings for the rows in the dataframe using the provided parameters.
+
+    Assigns the embeddings to the column "embeddings" in the dataframe as a side effect.
+
+    Args:
+        df (pd.DataFrame): The dataframe to be modified.
+        start (int, optional): The starting row. Defaults to 0.
+        num_rows (int, optional): The number of rows to process. Defaults to 5.
+    """
     futures = {}
-    for row in range(start, start+num_rows):
+    for row in range(start, start + num_rows):
         with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
             futures[row] = executor.submit(get_embedding, row, df.signature[row])
 
     for future in futures:
-        # if future % 5 == 0:
-            # print(futures[future].result()[0])
         row_no, embedding = futures[future].result()
         embedding = json.dumps(embedding)
-        print(row_no, embedding[0:10])
-        # df.embeddings[row_no] = embedding
-        # df.embeddings[futures[future].result()] = json.dumps(futures[future].result()[1])
+        logger.debug("row no {} embedding {}", row_no, embedding[0:10])
+        df.embeddings[row_no] = embedding
 
 
 def process_embeddings(df: pd.DataFrame):
@@ -104,26 +114,48 @@ def batch_process(df: pd.DataFrame, batch_size: int):
         batch_size (int): The batch size.
     """
     n_rows = df.shape[0]
-    batch_size = 10
     cycles = n_rows // batch_size
     extra = n_rows - cycles * batch_size
+
+    t0 = time.perf_counter()
     logger.info(
-        "Processing {} rows, divided into {} batches of size {}, followed by remaining {} rows", n_rows, cycles, batch_size, extra,
+        "Processing {} rows, divided into {} batches of size {}, followed by remaining {} rows",
+        n_rows,
+        cycles,
+        batch_size,
+        extra,
     )
     # process the batches up to the remainder
     if cycles > 0:
-        print(f"running from 0 to {cycles*batch_size} by {batch_size}")
+        logger.debug("running from 0 to {} by {}", cycles * batch_size, batch_size)
+        t1 =time.perf_counter()
         for start_index in range(0, cycles * batch_size, batch_size):
             if start_index >= n_rows:
                 break
             process_concurrently(df, start_index, batch_size)
-            if start_index % batch_size == 0:
+            if start_index > 0 and start_index % batch_size == 0:
                 logger.info(f"Processed {start_index} rows")
+        t2 = time.perf_counter()
+        logger.info("elapsed time for cycles: {:.3f}", t2-t1)
     # process the remainder
     if extra > 0:
-        print(f"extr cycles:{cycles} batch size {batch_size} extra {extra} cycles*batch_size {cycles*batch_size}")
+        logger.debug(
+            "extr cycles:{} batch size {} extra {} cycles*batch_size {}",
+            cycles,
+            batch_size,
+            extra,
+            cycles * batch_size,
+        )
+        t3 = time.perf_counter()
         process_concurrently(df, cycles * batch_size, extra)
-    logger.info("added {extra} rows from {cycles * batch_size} to {cycles * batch_size + extra}")
+        t4 = time.perf_counter()
+        logger.info("elapsed time for extras: {:.3f}", t4-t3)
+    logger.info(
+        "added {extra} rows from {cycles * batch_size} to {cycles * batch_size + extra}")
+    t5 = time.perf_counter()
+    logger.info("batch elapsed time {:.3f}", t5-t0)
+    
+
 
 def save_dataset(df: pd.DataFrame):
     """Save dataset to a csv, making a backup of the previous data file.
@@ -132,7 +164,7 @@ def save_dataset(df: pd.DataFrame):
         df (pd.DataFrame): The dataframe to save to disk.
     """
     data_file = Path(INPUT_FILE)
-    bu_file = Path(INPUT_FILE + '.bak')
+    bu_file = Path(INPUT_FILE + ".bak")
     logger.info("saving data file: {} backing up to: {}", data_file, bu_file)
     if data_file.exists():
         if bu_file.exists():
@@ -141,23 +173,28 @@ def save_dataset(df: pd.DataFrame):
     df.to_csv(data_file)
     logger.info("dataset saved to {}", data_file)
 
+
 def main():
     global api_key
-
+    start_time = time.perf_counter()
     logger.info("starting up")
     dotenv.load_dotenv(".env")
     api_key = os.getenv("OPENAI_API_KEY")
     logger.debug("API_KEY:{}", api_key)
     df = load_dataset()
-    df = df[df.index < 37]
-    # process_concurrently(df, 0,10)
+    loaded_time = time.perf_counter()
+    logger.info("loaded {} in {:.3f} seconds", df, (loaded_time - start_time))
+    # df = df[df.index < 37]
     batch_process(df, 100)
+    batch_time = time.perf_counter()
+    logger.info("completed batch processing in {:.3f} secondds", (batch_time - loaded_time))
     # print(df.head(20))
-    # save_dataset(df)
-    
+    save_dataset(df)
+    save_time = time.perf_counter()
+    logger.info("saved dataframe in {:.3f} seconds", (save_time - batch_time))
+    logger.info("total elapsed time {:.3f} seconds", (save_time - start_time))
     logger.info("shutting down")
 
 
 if __name__ == "__main__":
-    #     print("hi")
     main()
