ea68f67bb1e17e2f7c9776378ce61c4d9f24bc04
David Eidelman <eidelman.david@gmail.com>
2024-06-30 10:51:30 -0400

    moved get_apip_key to utils

--- src/ragsc/chroma.py
+++ src/ragsc/chroma.py
@@ -5,26 +5,28 @@ Interface to ChromaDB
 """
 
 from pathlib import Path
-from typing import Any, Union
+from typing import Union
 
 import chromadb
 import pandas as pd
 import json
 from loguru import logger
 
-def store_embeddings(collection: chromadb.Collection,
-                     df:pd.DataFrame, 
-                     min_item=0,
-                     max_item=-1,
-                     embeddings_column:str='embeddings',
-                     docs_column:str = 'cluster'
-                     )->int:
+
+def store_embeddings(
+    collection: chromadb.Collection,
+    df: pd.DataFrame,
+    min_item=0,
+    max_item=-1,
+    embeddings_column: str = "embeddings",
+    docs_column: str = "cluster",
+) -> int:
     if max_item == -1:
         max_item = df.shape[0]
     if max_item <= min_item:
         logger.error("max_item must be greater than min_item")
         return 0
-    docs =[]
+    docs = []
     embeds = []
     ids = []
     for i in range(min_item, max_item):
@@ -32,11 +34,7 @@ def store_embeddings(collection: chromadb.Collection,
         embeds.append(json.loads(df[embeddings_column].iloc[i]))
         ids.append(str(df.index[i]))
     try:
-        collection.add(
-            documents=docs,
-            embeddings=embeds,
-            ids = ids
-        )
+        collection.add(documents=docs, embeddings=embeds, ids=ids)
     except Exception as e:
         logger.error("unable to load data into database")
         logger.exception(e)
@@ -44,22 +42,26 @@ def store_embeddings(collection: chromadb.Collection,
     else:
         return max_item - min_item
 
-def initialize_database(collection_name:str = 'ragsc')-> chromadb.Collection:
+
+def initialize_database(collection_name: str = "ragsc") -> chromadb.Collection:
     client = chromadb.Client()
     return client.create_collection(collection_name)
 
-def load_data(input_path : Union[Path, str]) -> pd.DataFrame:
+
+def load_data(input_path: Union[Path, str]) -> pd.DataFrame:
     return pd.DataFrame()
 
-def main(data_path: Union[Path,str])->None:
+
+def main(data_path: Union[Path, str]) -> None:
     collection = initialize_database()
     logger.info("database initialized")
     df = load_data(data_path)
     logger.info("dataframe loaded")
-    n_stored = store_embeddings(collection=collection,df=df)
+    n_stored = store_embeddings(collection=collection, df=df)
     logger.info("stored {} embeddings", n_stored)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     input_path = Path("../../data/sigs.parquet")
-    assert(input_path.exists())
+    assert input_path.exists()
     main(input_path)
