a011ffa6fd10cdd794d1ba380c344548af24c54c
David Eidelman <eidelman.david@gmail.com>
2024-07-02 14:47:30 -0400

    refactored to use ragsc.signatures

--- subset.py
+++ subset.py
@@ -1,264 +1,20 @@
 """
-subset.py
+chroma.py
 
-script to provide a CLI for the work done in subset.ipynb
+CLI to process data from the equivalent of subset.adata, generate vector database, and test it.
 
 Copyright 2024 David Eidelman, MIT License.
 """
 
 import sys
 from pathlib import Path
-from typing import Any
 
 import anndata as ad
 import click
 import pandas as pd
 from loguru import logger
-from ragsc import utils
+from ragsc import signatures, utils
 from ragsc import embed as em
-
-
-def get_cluster_names(adata: ad.AnnData, criterion="leiden_2") -> list[str]:
-    """
-    get_cluster_names
-
-    Get the cluster names assigned by the given criterion.
-
-    Args:
-        adata (ad.AnnData): The input.
-        criterion (str, optional): The criterion column used to cluster the data. Defaults to "leiden_2".
-
-    Returns:
-        list[str]: The list of cluster names.
-    """
-    clusters = [
-        str(x)
-        for x in sorted([int(cluster) for cluster in adata.obs[criterion].unique()])
-    ]
-    return clusters
-
-
-def partition_clusters(
-    adata: ad.AnnData, criterion="leiden_2"
-) -> dict[str, ad.AnnData]:
-    """
-    partition_clusters
-    Divide up the data into a series of subset AnnDatas stored in a dictionary
-    where the key is the cluster name.
-
-    Calls get_cluster_names
-
-    Args:
-        adata (ad.AnnData): The input.
-        criterion (str, optional): The column containing the cluster information. Defaults to "leiden_2".
-
-    Returns:
-        dict[str, ad.AnnData]: A dictionary with cluster names as keys and data in AnnData format.
-    """
-    clusters = get_cluster_names(adata, criterion)
-    cluster_table: dict[str, Any] = {}
-    for cluster in clusters:
-        subset = adata[adata.obs[criterion] == cluster]  # type: ignore
-        cluster_table[cluster] = subset.copy()
-    return cluster_table
-
-
-def calculate_highest_frequency_genes(
-    adata: ad.AnnData,
-    number_of_genes: int = 20,
-    expression_threshold=0.0,
-    verbose=False,
-) -> list[str]:
-    """
-    calculate_highest_frequency_genes
-
-    Determine the genes that are most frequently in cells.
-
-    Args:
-        adata (ad.AnnData): The input.
-        number_of_genes (int, optional): The threshold to be called high frequency. Defaults to 20.
-        expression_threshold (float, optional): The minimum expression level for a gene to be considered. Defaults to 0.0.
-        verbose (bool, optional): Prints additional information, if True. Defaults to False.
-
-    Returns:
-        list[str]: A list of gene names representing the genes expressed in the most cells.
-    """
-    cell_count, gene_count = adata.shape
-    if verbose:
-        logger.debug(f"{cell_count} cells, {gene_count} genes")
-    gene_table = {}
-
-    # create a table with the genes as the keys and frequency as the value
-    for cell_no in range(cell_count):
-        b = adata.X[cell_no] > expression_threshold  # type: ignore
-        genes = adata.var.index[b]
-        for gene in genes:
-            if gene in gene_table:
-                gene_table[gene] += 1
-            else:
-                gene_table[gene] = 1
-    # set the table to be in descending order
-    gene_table = dict(sorted(gene_table.items(), key=lambda x: x[1], reverse=True))
-
-    # create a list of the most frequently expressed in cells
-    gene_list = list(gene_table.keys())[0:number_of_genes]
-    if verbose:
-        for gene in gene_list:
-            logger.debug(
-                f"{gene}:{gene_table[gene]} ({gene_table[gene]/cell_count*100:4.1f}%)"
-            )
-    return gene_list
-
-
-def find_common_genes(
-    adata: ad.AnnData,
-    cluster_table: dict[str, ad.AnnData],
-    genes_per_cluster=25,
-    repeat_limit=5,
-    expression_threshold=0.0,
-) -> set[str]:
-    """
-    find_common_genes
-
-    Find genes that are almost ubiquitously expressed.
-
-    Args:
-        adata (ad.AnnData): The input.
-        genes_per_cluster (int, optional): The number of genes per cluster to consider. Defaults to 25.
-        repeat_limit (int, optional): The maximum number of genes allowed in multiple clusters. Defaults to 5.
-        expression_threshold (float, optional): The minimum gene expression threshold. Defaults to 0.0.
-
-    Returns:
-        set[str]: A set of genes that are widely expressed for potential elimination from gene signatures.
-    """
-    gene_dict = {}
-    for cluster in cluster_table:
-        cluster_adata = cluster_table[cluster]
-        gene_list = calculate_highest_frequency_genes(
-            adata=cluster_adata,
-            number_of_genes=genes_per_cluster,
-            expression_threshold=expression_threshold,
-        )
-        # record which clusters express each gene
-        for gene in gene_list:
-            if gene in gene_dict:
-                gene_dict[gene].append(cluster)
-            else:
-                gene_dict[gene] = [cluster]
-    # filter out genes that are only present in a few clusters
-    gene_dict = {k: v for k, v in gene_dict.items() if len(v) >= repeat_limit}
-
-    # get the resulting list of gene names
-    gene_names = list(gene_dict.keys())
-    common_genes = set(gene_names)
-
-    return common_genes
-
-
-def get_gene_signature(
-    adata: ad.AnnData,
-    feature_index: int,
-    expression_threshold=0.0,
-    redundant_genes: set[str] = set(),
-    verbose: bool = False,
-) -> dict[str, float]:
-    """Calculate the list of genes for the given cell based on the feature_index.  Only non-mitochondrial
-    genes with an expression level greater than the provided expression threshold are reported.
-
-    Args:
-        adata (ad.AnnData): The AnnData containing the cells.  Assume that only highly variable genes have been provided.
-        feature_index (int): The index of the cell to be measured
-        expression_threshold (float, optional): The minimum expression level. Defaults to 0.0, which returns all non-zero genes.
-        redundant_genes (set[str], optional): Genes to be filtered out from the final result. Defaults to the empty set.
-        verbose (bool, optional): Print intermediated data to standard output. Defaults to False.
-    Returns:
-        dict[str, float]: Dictionary with gene names as keys and expression as values.
-    """
-    num_cells, _ = adata.shape
-    if feature_index < 0 or feature_index > num_cells - 1:
-        raise ValueError(
-            f"Feature index ({feature_index}) outside the range of cells (0..{num_cells-1}) in the current dataset."
-        )
-    gene_data = adata.X[feature_index].copy()  # type: ignore
-    if verbose:
-        logger.debug(f"Started with {len(gene_data)} genes")
-    # calculate the mask to find gene subset
-    b = gene_data > expression_threshold
-    expression = gene_data[b]
-    names = adata.var.index[b]
-    assert expression.shape == names.shape
-    if verbose:
-        logger.debug(f"Found {len(names)} genes exceeding expression threshold.")
-
-    # sort the genes based on expression
-    genes = dict(
-        sorted(dict(zip(names, expression)).items(), key=lambda x: x[1], reverse=True)
-    )
-    # remove redundant and mitochondrial genes
-    genes = {
-        k: v
-        for k, v in genes.items()
-        if k not in redundant_genes and not k.startswith("MT-")
-    }
-    if verbose:
-        logger.debug(f"Found {len(genes)} genes after filtering.")
-    return genes
-
-
-def process_clusters(
-    cluster_table: dict[str, ad.AnnData],
-    redundant_genes: set[str],
-    expression_threshold=0.0,
-    verbose=False,
-) -> pd.DataFrame:
-    """Given a dictionary of clusters and a set of redundant genes, calculates the gene_signature on a cell by cell basis.
-
-    Args:
-        cluster_table (dict[str,ad.AnnData]): Holds the cluster data with cluster names as keys.
-        redundant_genes (set[str]): A set of separately calculated genes to exclude from signatures.
-        expression_threshold (float, optional): Only count genes with expression levels greater than this number. Defaults to 0.0.
-        verbose (bool, optional): Defaults to False.
-
-    Returns:
-        pd.DataFrame: A dataframe with cells as the index and columns for cluster name and signature (as a space separated string).
-    """
-    dataframes: list[pd.DataFrame] = []
-    total_cells = 0
-    for cluster_name in cluster_table:
-        cluster_signatures = pd.DataFrame(
-            columns=["cluster", "signature"],
-            index=cluster_table[cluster_name].obs.index,
-        )
-        cluster_adata = cluster_table[cluster_name]
-        n_cells, _ = cluster_adata.shape
-        if verbose:
-            logger.debug(f"cluster {cluster_name} has {n_cells} cells.")
-        total_cells += n_cells
-        for cell_no in range(n_cells):
-            cluster_signatures.iloc[cell_no, 0] = cluster_name
-            cluster_signatures.iloc[cell_no, 1] = " ".join(
-                list(
-                    get_gene_signature(
-                        adata=cluster_adata,
-                        feature_index=cell_no,
-                        expression_threshold=expression_threshold,
-                        redundant_genes=redundant_genes,
-                    ).keys()
-                )
-            )
-        if verbose:
-            logger.debug(cluster_signatures.head())
-        dataframes.append(cluster_signatures)
-
-    sigs = pd.concat(dataframes, axis=0)
-    assert (
-        sigs.shape[0] == total_cells
-    )  # sanity check to ensure that all cells are being processed
-    if verbose:
-        logger.debug(
-            f"Processed {total_cells} to produce a dataframe with dimensions {sigs.shape}."
-        )
-    return sigs
 
 
 def process_cluster_data(adata: ad.AnnData, expression_threshold=1.5) -> pd.DataFrame:
@@ -269,19 +25,19 @@ def process_cluster_data(adata: ad.AnnData, expression_threshold=1.5) -> pd.Data
     logger.info(
         "processing {} cells {} highly variable genes", adata.shape[0], adata.shape[1]
     )
-    cluster_names = get_cluster_names(adata)
+    cluster_names = signatures.get_cluster_names(adata)
     logger.info("found {} clusters", len(cluster_names))
-    cluster_table = partition_clusters(adata)
+    cluster_table = signatures.partition_clusters(adata)
     logger.info(
         "partitioned adata into dict with {} adatas as values, one per cluster",
         len(cluster_table),
     )
-    redundant_genes = find_common_genes(
-        adata, cluster_table=cluster_table, expression_threshold=expression_threshold
+    redundant_genes = signatures.find_redundant_genes(
+        cluster_table=cluster_table, expression_threshold=expression_threshold
     )
     logger.info("found {} redundant genes", len(redundant_genes))
 
-    df = process_clusters(
+    df = signatures.process_clusters(
         cluster_table=cluster_table,
         redundant_genes=redundant_genes,
         expression_threshold=expression_threshold,
