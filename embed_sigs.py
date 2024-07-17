6567b03f6e05ac26faed81a32e2a54959fe314b1
deidelma <eidelman.david@gmail.com>
2024-07-17 00:06:41 +0000

    better handling for empty rows

--- embed_sigs.py
+++ embed_sigs.py
@@ -31,10 +31,11 @@ def embed_rows(df: pd.DataFrame, api_key: str, n_rows: int = -1) -> pd.DataFrame
         logger.info("processing entire dataframe")
     logger.info("attempting to embed {} rows", n_rows)
     t1 = time.perf_counter()
+    rows_embedded = 0
     for i in range(n_rows):
         sig = df.signature.iloc[i]
+        # skip empty signatures
         if sig is None or len(sig) == 0:
-            logger.error("undefined signature for row {}",i)
             continue
         row_no, embedding = embed.get_embedding(
             cell_no=i, gene_signature=sig, api_key=api_key
@@ -44,6 +45,7 @@ def embed_rows(df: pd.DataFrame, api_key: str, n_rows: int = -1) -> pd.DataFrame
         assert "embeddings" in df.columns
         assert isinstance(embedding, str)
         df.embeddings.iloc[row_no] = embedding
+        rows_embedded += 1
         if row_no % 10 == 0:
             logger.info("processed row {}", row_no)
     t2 = time.perf_counter()
@@ -52,15 +54,19 @@ def embed_rows(df: pd.DataFrame, api_key: str, n_rows: int = -1) -> pd.DataFrame
     logger.info(
         "embeddings complete, elapsed time: {0:7.2f}, time_per_iteration: {1:5.3f}".format(
             elapsed, time_per_row
-        )
-    )
-    logger.info("completed embedding of {} rows", n_rows)
+        ))
+    logger.info("input: {} rows, embedded: {} rows, null: {} rows", n_rows, rows_embedded, n_rows-rows_embedded)
     return df
+
+def get_output_file_path(input_file) -> Path:
+    inpath = Path(input_file)
+    return inpath.with_suffix('.csv.gz')
 
 def process_file(input_file_name):
     input_file_path = Path(input_file_name)
     assert input_file_path.exists()
-    output_file_path = input_file_path.with_suffix('.csv.gz')
+    # output_file_path = input_file_path.with_suffix('.csv.gz')
+    output_file_path = get_output_file_path(input_file_path)
     df = pd.read_parquet(input_file_path)
     logger.info("dataframe loaded from {}", input_file_path)
 
@@ -72,7 +78,7 @@ def process_file(input_file_name):
     df.to_csv(output_file_path, index_label="cell_no",compression='gzip')
     logger.info("wrote updated dataframe to {}", output_file_path)
 
-def process_directory(directory_name):
+def process_directory(directory_name, overwrite = False):
     dir_path = Path(directory_name)
     logger.info("processing directory {}", dir_path)
     if not dir_path.exists() or not dir_path.is_dir():
@@ -80,32 +86,42 @@ def process_directory(directory_name):
         sys.exit(1)
 
     for f in dir_path.iterdir():
+        output_path = get_output_file_path(f)
+        if not overwrite and output_path.exists(): 
+            logger.info("skipping file {} as it's output file {} already exists",f, output_path)
+            continue
         # if f.name != INPUT_FILE_PATH.name and f.suffix == '.parquet':
         if f.suffix == '.parquet':
             try:
                 logger.info("about to process file {}", f)
                 process_file(f)
+                logger.info("processed file: {}",f)
             except Exception as e:
-                logger.error("unable to process file: {}", f)
+                logger.error("unable to process file: {} due to exception", f)
                 logger.exception(e)
+        else:
+            logger.error("file {} does not seem to be a parquet file - unable to process")
 
 
 @click.command()
-@click.option("-f", "--filename")
-@click.option("-d","--directory")
+@click.option("-f", "--filename", help="set the input filename")
+@click.option("-d","--directory", help="set the input directory")
+@click.option("--overwrite/--no-overwrite", default=True, help="if True, ignores existing output files ")
 def embed_sigs(**kwargs):
     if kwargs['directory'] is not None:
-        # print("....")
         dirpath = Path(kwargs['directory'])
     else:
         dirpath = RESULTS_PATH 
     if kwargs['filename'] is not None:
         filepath = Path(kwargs['filename'])
-        print(f"single file: {kwargs['filename']}")
+        logger.info("processing file: {}", dirpath / filepath)
         process_file(dirpath / filepath)
+        logger.info("file processing complete")
     else:
+        logger.info("processing directory:{}")
         process_directory(dirpath)
+        logger.info("directory processing complete")
+
 if __name__ == "__main__":
     logger.add("logs/sig_embed_{time}.log")
     embed_sigs()
-    # process_directory(RESULTS_PATH)
